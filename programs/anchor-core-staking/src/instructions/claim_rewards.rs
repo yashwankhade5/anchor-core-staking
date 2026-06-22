36fd3859d4daf5698c5683d9df2b3fd4f296d1a7
Yash Wankhade <yashwankhade5@gmail.com>
2026-06-22 06:58:02 -0700

    claim reward

---
+++ programs/anchor-core-staking/src/instructions/claim_rewards.rs
@@ -0,0 +1,56 @@
+use crate::errors::StakingError;
+use crate::state::Config;
+use anchor_lang::prelude::*;
+use anchor_spl::{
+    associated_token::AssociatedToken,
+    token_interface::{mint_to_checked, Mint, MintToChecked, TokenAccount, TokenInterface},
+};
+use mpl_core::{
+    accounts::{BaseAssetV1, BaseCollectionV1},
+    fetch_plugin,
+    instructions::UpdatePluginV1CpiBuilder,
+    types::{Attribute, Attributes, Plugin, PluginType, UpdateAuthority},
+    ID as MPL_CORE_ID,
+};
+
+#[derive(Accounts)]
+pub struct ClaimRewards<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+    /// CHECK: PDA Update authority
+    #[account(
+        seeds = [b"update_authority", collection.key().as_ref()],
+        bump
+    )]
+    pub update_authority: UncheckedAccount<'info>,
+    #[account(
+        seeds = [b"config", collection.key().as_ref()],
+        bump = config.config_bump
+    )]
+    pub config: Account<'info, Config>,
+    #[account(
+        mut,
+        seeds = [b"rewards", config.key().as_ref()],
+        bump = config.rewards_bump
+    )]
+    pub rewards_mint: InterfaceAccount<'info, Mint>,
+    #[account(
+        init_if_needed,
+        payer = user,
+        associated_token::mint = rewards_mint,
+        associated_token::authority = user,
+    )]
+    pub user_rewards_ata: InterfaceAccount<'info, TokenAccount>,
+    /// CHECK: NFT account will be checked by the mpl core program
+    #[account(mut)]
+    pub nft: UncheckedAccount<'info>,
+    /// CHECK: Collection account will be checked by the mpl core program
+    #[account(mut)]
+    pub collection: UncheckedAccount<'info>,
+    /// CHECK: This is the ID of the Metaplex Core program
+    #[account(address = MPL_CORE_ID)]
+    pub mpl_core_program: UncheckedAccount<'info>,
+    pub system_program: Program<'info, System>,
+    pub token_program: Interface<'info, TokenInterface>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+}
--- programs/anchor-core-staking/src/instructions/mod.rs
+++ programs/anchor-core-staking/src/instructions/mod.rs
@@ -3,11 +3,11 @@ pub mod stake;
 pub mod unstake;
 pub mod create_collection;
 pub mod mint_asset;
-// pub mod claim_rewards;
+pub mod claim_rewards;
 
 pub use initialize::*;
 pub use stake::*;
 pub use unstake::*;
 pub use create_collection::*;
 pub use mint_asset::*;
-// pub use claim_rewards::*;
+pub use claim_rewards::*;
